ac41216e5e20f9f57f5c0c0b57206546f3bc5f79
uCeeya <cyalwa@gmail.com>
2025-01-26 20:57:41 +0200

    Added Comments on the test cases

--- tests/automationExerciseTests.spec.ts
+++ tests/automationExerciseTests.spec.ts
@@ -1,20 +1,23 @@
 import { test, expect } from '@playwright/test';
-import { title } from 'process';
 
+// This launches the browser and navigates to the homepage before each test
 test.beforeEach(async ({ page }) => {
   await page.goto('https://automationexercise.com/');
 });
+
+// This test verifies that the homepage loads correctly
 test('Verify homepage loads correctly', async ({ page }) => {
   await expect(page).toHaveTitle('Automation Exercise');
 });
 
+// This test verifies that the products page loads correctly and all products are displayed
 test('Check navigation to Products page', async ({ page }) => {
   await page.click('a[href="/products"]');
   await expect(page).toHaveURL(/.*\/products/);
   await expect(page.locator('h2.title')).toHaveText('All Products');
 });
 
-
+// This test verifies that the user is able to search for a product
 test('Search for a product', async ({ page }) => {
   await page.click('a[href="/products"]');
   await page.fill('input[id="search_product"]', 'Blue Top');
@@ -22,6 +25,7 @@ test('Search for a product', async ({ page }) => {
   await expect(page.locator('.productinfo')).toContainText('Blue Top');
 });
 
+// This test checks the user login with valid credentials and valid message is displayed
 test('User login with invalid credentials - Case1', async ({ page }) => {
   await page.click('a[href="/login"]');
   await page.fill('input[data-qa="login-email"]', 'invalid@example.com');
@@ -30,6 +34,7 @@ test('User login with invalid credentials - Case1', async ({ page }) => {
   await expect(page.locator('.login-form p')).toHaveText('Your email or password is incorrect!');
 });
 
+// This test creates the user and checks the successful registration message
 test('Successful User Registration', async ({ page }) => {
   await page.click('a[href="/login"]');
   await page.fill('input[data-qa="signup-email"]', `test${Date.now()}@example.com`);
@@ -52,3 +57,5 @@ test('Successful User Registration', async ({ page }) => {
   await page.click('button[data-qa="create-account"]');
   await expect(page.locator('b').filter({ hasText: 'Account Created' })).toBeVisible();
 });
+
+
